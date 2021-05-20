998bc2a9f046d861d9fb3c60838f0f8a2b0720d6
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-05-20 04:26:11 +0100

    note.js file created and api routres are pushed in

---
+++ Develop/src/controllers/notes.js
@@ -0,0 +1,14 @@
+const getAllNotes = (req, res) => {};
+
+const getNoteById = (req, res) => {};
+
+const createNote = (req, res) => {};
+
+const deleteNote = (req, res) => {};
+
+module.exports = {
+  getAllNotes,
+  getNoteById,
+  createNote,
+  deleteNote,
+};
