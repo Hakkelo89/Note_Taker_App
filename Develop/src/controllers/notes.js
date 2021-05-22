375a72a0745af3a8a6431b4f97f5fe2576ebb739
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-05-22 10:25:04 +0100

    note.js file updated

--- Develop/src/controllers/notes.js
+++ Develop/src/controllers/notes.js
@@ -1,10 +1,59 @@
-const getAllNotes = (req, res) => {};
+const fs = require("fs");
+const getFromDb = require("../utils/getFromDb");
+const writeToDb = require("../utils/writeToDb");
 
-const getNoteById = (req, res) => {};
+const getAllNotes = (req, res) => {
+  const notes = getFromDb();
 
-const createNote = (req, res) => {};
+  res.json(notes);
+};
 
-const deleteNote = (req, res) => {};
+const getNoteById = (req, res) => {
+  const { id } = req.params;
+  const notes = getFromDb();
+
+  const note = notes[id];
+
+  res.json(note);
+};
+
+const createNote = (req, res) => {
+  const id = 2;
+
+  const { body } = req;
+  const { text, title } = body;
+  console.log(id, "id");
+  console.log(body, "body");
+
+  let notes = getFromDb();
+  console.log(notes, "notes");
+
+  const newNote = {
+    id,
+    text,
+    title,
+  };
+  notes.push(newNote);
+  console.log(newNote, "newNote");
+
+  const callback = (err) => {
+    if (err) {
+      console.log("fail", err);
+    } else {
+      console.log("pass");
+    }
+  };
+
+  notes = JSON.stringify(notes);
+
+  fs.writeFile("/db/db.json", notes, callback);
+};
+
+const deleteNote = (req, res) => {
+  notes, newNote, writeToDb(JSON.stringify(newNotes));
+
+  res.json({ success: true });
+};
 
 module.exports = {
   getAllNotes,
